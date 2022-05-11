0c126dfda30b786fc1cbb9e191cba521432c7ac7
Ivan Ermacoff <jermacoff@gmail.com>
2022-05-11 22:03:30 +0700

    Switch to Tokyo Night theme

--- .config/nvim/lua/cosmic/config/editor.lua
+++ .config/nvim/lua/cosmic/config/editor.lua
@@ -2,7 +2,14 @@
 
 local g = vim.g
 local map = require('cosmic.utils').map
+local o = vim.o
+local wo = vim.wo
 local opt = vim.opt
 
 -- Set GUI font
 opt.guifont = { "FiraCode NF", "h12" }
+
+-- Enable scrolloff beyond  last line
+o.scrolloff = 5
+wo.scrolloff = 5
+opt.scrolloff = 5
