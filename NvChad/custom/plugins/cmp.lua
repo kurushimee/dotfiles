a8dfbc0dfd6ca299c194d39cafc7645311837ac3
Ivan Ermakov <iver00@ya.ru>
2022-09-15 15:18:40 +0700

    Configure NvChad

---
+++ NvChad/custom/plugins/cmp.lua
@@ -0,0 +1,20 @@
+local present, cmp = pcall(require, "cmp")
+
+if not present then
+  return
+end
+
+-- Command line completion
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = { { name = "buffer" } },
+})
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
--- NvChad/custom/plugins/init.lua
+++ NvChad/custom/plugins/init.lua
@@ -1,6 +1,55 @@
+local overrides = require "custom.plugins.overrides"
+
 return {
+  ["nvim-telescope/telescope.nvim"] = {
+    override_options = overrides.telescope
+  },
+
   ["goolord/alpha-nvim"] = {
     disable = false,
-    cmd = "Alpha",
+    override_options = overrides.alpha
   },
+
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+
+  ["ray-x/cmp-treesitter"] = {},
+  ["hrsh7th/cmp-cmdline"] = {},
+  ["hrsh7th/nvim-cmp"] = {
+    override_options = overrides.cmp,
+    config = function()
+      require "plugins.configs.cmp"
+      require "custom.plugins.cmp"
+    end,
+  },
+
+  ["nvim-treesitter/nvim-treesitter"] = {
+    override_options = overrides.treesitter,
+  },
+
+  ["williamboman/mason.nvim"] = {
+    override_options = overrides.mason,
+  },
+
+  ["kyazdani42/nvim-tree.lua"] = {
+    override_options = overrides.nvimtree,
+  },
+
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+
+  ["ahmedkhalf/project.nvim"] = {
+    cmd = "Telescope project",
+    config = function()
+      require("project_nvim").setup()
+    end
+  }
 }
